1a3d51a2d99a6d9b103b4702081053dddfc191e0
April Hughes <dreydain@gmail.com>
2022-11-03 12:27:46 -0700

    adjusted grid spacing and added inline mui styling

--- src/sections/phone-demo/PhoneDemo.jsx
+++ src/sections/phone-demo/PhoneDemo.jsx
@@ -7,16 +7,21 @@ import Grid from '@mui/material/Grid';
 
 const PhoneDemo = () => {
 	return (
-		<Grid className='phone-body' container spacing={2}>
-			<Grid item xs='auto'>
+		<Grid
+			className='phone-body'
+			container
+			spacing={2}
+			sx={{ justifyContent: 'center', textAlign: 'center' }}
+		>
+			<Grid item xs={4}>
 				<h2>1. Find</h2>
 				<img src={PhonePreview} alt='Phone Demo Preview'></img>
 			</Grid>
-			<Grid item xs='auto'>
+			<Grid item xs={4}>
 				<h2>2. Eat</h2>
 				<img src={PhoneSearch} alt='Phone Demo Search'></img>
 			</Grid>
-			<Grid item xs='auto'>
+			<Grid item xs={4}>
 				<h2>3. More</h2>
 				<img src={PhoneAbout} alt='Phone Demo About'></img>
 			</Grid>
