ed711169ec7cffcb8b583b250ec72ec0db35ab57
April Hughes <dreydain@gmail.com>
2022-11-07 11:49:40 -0800

    made responsive

--- src/sections/phone-demo/PhoneDemo.jsx
+++ src/sections/phone-demo/PhoneDemo.jsx
@@ -11,19 +11,23 @@ const PhoneDemo = () => {
 			className='phone-body'
 			container
 			spacing={2}
-			sx={{ justifyContent: 'center', textAlign: 'center' }}
+			sx={{
+				justifyContent: 'center',
+				textAlign: 'center',
+				padding: '3rem',
+			}}
 		>
-			<Grid item xs={4}>
+			<Grid item xs={12} md={4}>
 				<h2>1. Find</h2>
-				<img src={PhonePreview} alt='Phone Demo Preview'></img>
+				<img src={PhonePreview} Height='460rem' alt='Phone Demo Preview'></img>
 			</Grid>
-			<Grid item xs={4}>
+			<Grid item xs={12} md={4}>
 				<h2>2. Eat</h2>
-				<img src={PhoneSearch} alt='Phone Demo Search'></img>
+				<img src={PhoneSearch} Height='460rem' alt='Phone Demo Search'></img>
 			</Grid>
-			<Grid item xs={4}>
+			<Grid item xs={12} md={4}>
 				<h2>3. More</h2>
-				<img src={PhoneAbout} alt='Phone Demo About'></img>
+				<img src={PhoneAbout} Height='460rem' alt='Phone Demo About'></img>
 			</Grid>
 		</Grid>
 	);
