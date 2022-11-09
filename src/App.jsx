81b1a655dca0c7473f9d84d6de95ee7812cd49d9
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-11-08 18:55:44 -0600

    Added About section to main app file, and blocked out main css features.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PhoneDemo from "./sections/phone-demo/PhoneDemo";
-import Contact from "./sections/contact/Contact";
-import Contributors from "./sections/contributors/Contributors";
+import Contact from "./sections/Contact/Contact";
+import Contributors from "./sections/Contributors/Contributors";
 import MaterialUIExample from './MaterialUiExample'
+import About from './sections/About/About'
 
 const App = () => {
 	return (
 		<div>
+			<About />
 			<PhoneDemo />
 			<Contact />
 			<Contributors />
--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -4,7 +4,16 @@ import './About.css';
 const About = () => {
     return (
         <>
-        
+            <section>
+                <div className='about-text'>
+                    <h2>Connecting those in Need</h2>
+                    <p>When one of us has food insecurity, it is a problem for all of us. Let’s change that, one meal at a time. <br /><br />
+                         We have too much abundance to let each other go hungry, period. </p>
+                </div>
+                <div className='image'>
+                    <p>THIS WILL BE AN IMAGE THIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGE</p>
+                </div>
+            </section>
         </>
     )
 }
