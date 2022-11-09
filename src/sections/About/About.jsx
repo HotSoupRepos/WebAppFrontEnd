58eb9228cc3512deb42e648ee068eb0708eaa163
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-11-08 19:18:51 -0600

    Added apple/google store images to about block and added minimal styling.

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './About.css';
+import AppleStore from '../../assets/images/AppleStore.png'
+import GooglePlayStore from '../../assets/images/GooglePlayStore.png'
 
 const About = () => {
     return (
@@ -9,6 +11,8 @@ const About = () => {
                     <h2>Connecting those in Need</h2>
                     <p>When one of us has food insecurity, it is a problem for all of us. Let’s change that, one meal at a time. <br /><br />
                          We have too much abundance to let each other go hungry, period. </p>
+                    <img src={AppleStore}/>
+                    <img src={GooglePlayStore}/>
                 </div>
                 <div className='image'>
                     <p>THIS WILL BE AN IMAGE THIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGE</p>
