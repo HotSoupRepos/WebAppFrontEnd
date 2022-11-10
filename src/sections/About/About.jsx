33ba1d6122bc87f55cc0e89205965d0320197b69
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-11-09 20:19:04 -0600

    Added Image for User profile/persona and further adjusted styles

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import './About.css';
 import AppleStore from '../../assets/images/AppleStore.png'
 import GooglePlayStore from '../../assets/images/GooglePlayStore.png'
+import UserProfile from '../../assets/images/UserProfile.png'
 
 const About = () => {
     return (
         <>
             <section>
                 <div className='about-text'>
-                    <h2>Connecting those in Need</h2>
+                    <h1>Connecting those in Need</h1>
                     <p>When one of us has food insecurity, it is a problem for all of us. Let’s change that, one meal at a time. <br /><br />
                          We have too much abundance to let each other go hungry, period. </p>
-                    <img src={AppleStore}/>
-                    <img src={GooglePlayStore}/>
+                    <img src={AppleStore} alt='Apple store logo'/>
+                    <img src={GooglePlayStore} alt='Google Play store logo'/>
                 </div>
                 <div className='image'>
-                    <p>THIS WILL BE AN IMAGE THIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGETHIS WILL BE AN IMAGE</p>
+                    <img src={UserProfile}/>
                 </div>
             </section>
         </>
