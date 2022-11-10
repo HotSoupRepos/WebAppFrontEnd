d788a5c894d54cabca27668dbcae757ed1f96725
April Hughes <dreydain@gmail.com>
2022-11-09 16:23:30 -0800

    improved img responsiveness and edited text

--- src/sections/phone-demo/PhoneDemo.jsx
+++ src/sections/phone-demo/PhoneDemo.jsx
@@ -5,7 +5,6 @@ import PhonePreview from '../../assets/images/PhonePreview.png';
 import PhoneAbout from '../../assets/images/PhoneAbout.png';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
-import { Image } from '@mui/icons-material';
 
 const PhoneDemo = () => {
 	return (
@@ -16,19 +15,32 @@ const PhoneDemo = () => {
 			sx={{
 				textAlign: 'center',
 				padding: '2rem 0 3rem 0',
+				width: '100vw',
 			}}
 		>
 			<Grid item xs={12} md={4}>
-				<Typography variant='h5'>1. Find</Typography>
-				<img src={PhonePreview} Height='400rem' alt='Phone Demo Preview'></img>
+				<Typography variant='h4'>1.Find</Typography>
+				<img
+					src={PhonePreview}
+					style={{ height: 'auto', width: '55%' }}
+					alt='Phone Demo Preview'
+				></img>
 			</Grid>
 			<Grid item xs={12} md={4}>
-				<Typography variant='h5'>2. Eat</Typography>
-				<img src={PhoneSearch} Height='400rem' alt='Phone Demo Search'></img>
+				<Typography variant='h4'>2.Eat</Typography>
+				<img
+					src={PhoneSearch}
+					style={{ height: 'auto', width: '55%' }}
+					alt='Phone Demo Search'
+				></img>
 			</Grid>
 			<Grid item xs={12} md={4}>
-				<Typography variant='h5'>3. More</Typography>
-				<img src={PhoneAbout} Height='400rem' alt='Phone Demo About'></img>
+				<Typography variant='h4'>3.More</Typography>
+				<img
+					src={PhoneAbout}
+					style={{ height: 'auto', width: '55%' }}
+					alt='Phone Demo About'
+				></img>
 			</Grid>
 		</Grid>
 	);
