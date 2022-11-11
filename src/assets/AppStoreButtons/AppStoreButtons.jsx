c9c418f46ccd2dbae292316cbd5d19ff0df22131
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-11-10 23:51:04 -0600

    Created handle click for eventual functionality to lead to App store. The functionality will need to be reworked in the furture

--- src/assets/AppStoreButtons/AppStoreButtons.jsx
+++ src/assets/AppStoreButtons/AppStoreButtons.jsx
@@ -4,11 +4,21 @@ import AppleStore from '../../assets/images/AppleStore_DM.png'
 import GooglePlayStore from '../../assets/images/GooglePlayStore_DM.png'
 
 function AppStoreButtons () {
+
+    function handleClick(appStore) {
+        if (appStore == 'Google'){
+            alert("This will one day lead to the Play store!")
+        }
+        else if (appStore == 'Apple'){
+            alert("This will one day lead to the IOS store!")
+        }
+    }
+
     return (
         < >
         <div className='app-buttons'>
-            <img src={AppleStore} alt='Apple store logo'/>
-            <img src={GooglePlayStore} alt='Google Play store logo'/>
+            <img src={AppleStore} onClick={()=>handleClick('Apple')} alt='Apple store logo'/>
+            <img src={GooglePlayStore} onClick={()=>handleClick('Google')} alt='Google Play store logo'/>
         </div>
         </>
     )
