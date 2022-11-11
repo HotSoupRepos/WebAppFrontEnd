c022a2dcecacefd0ce260cdcef6f61efbee099ee
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-11-11 17:29:57 -0500

    Update data.js with contributors on file

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export default [
 		id: 1,
 		headshot: "https://avatars.githubusercontent.com/u/99847030?v=4",
 		name: "Jasmine Vo",
-		title: "Frontend Engineer",
+		title: "Frontend-Web",
 		linkedin: "https://www.linkedin.com/in/jasminepvo/",
 		github: "https://github.com/jasminepvo",
 		discord: "discordapp.com/users/jasminepvo#7016",
@@ -13,7 +13,7 @@ export default [
 		headshot:
 			"https://avatars.githubusercontent.com/u/100975544?s=400&u=1b1f1ff379261638681e068aab1151c172799f9b&v=4",
 		name: "Sierra Green",
-		title: "Website Developer",
+		title: "Frontend-Web",
 		linkedin: "https://www.linkedin.com/in/sierragreen379/",
 		github: "https://github.com/sierragreen379",
 		discord: "discordapp.com/users/Sierra#2268",
@@ -27,4 +27,63 @@ export default [
 		github: "https://github.com/ziraserrano",
 		discord: "discordapp.com/users/zira#7437",
 	},
+	{
+		id: 4,
+		headshot:
+			"https://media-exp1.licdn.com/dms/image/C4D03AQERENoZ7j9kNg/profile-displayphoto-shrink_800_800/0/1658257728065?e=1673481600&v=beta&t=j0GS3k6vpqa44l7cXZOZdSL4bkhwEYh8aZVZ8s6kyBk",
+		name: "Rafaela Grison",
+		title: "Backend/data",
+		linkedin: "https://www.linkedin.com/in/rafaelagrison/",
+		github: "",
+		discord: "",
+	},
+	{
+		id: 5,
+		headshot:
+			"https://media-exp1.licdn.com/dms/image/D4E03AQHba2O2KcLr-g/profile-displayphoto-shrink_800_800/0/1667356156231?e=1673481600&v=beta&t=7kVGGig5MAbenT4RILePUTk4E1PY9W-IZOJLCh_2ymo",
+		name: "Deja Drummer",
+		title: "Lead Web Developer",
+		linkedin: "https://www.linkedin.com/in/drummer5/",
+		github: "",
+		discord: "",
+	},
+	{
+		id: 6,
+		headshot:
+			"https://media-exp1.licdn.com/dms/image/C5603AQGFBvS3PGiJ3w/profile-displayphoto-shrink_800_800/0/1654723721006?e=1673481600&v=beta&t=yAzf0CBlH39grp159GmwQI1Xidxi1vGXeVybHgnN9SY",
+		name: "Ariel Steger",
+		title: "",
+		linkedin: "",
+		github: "",
+		discord: "",
+	},
+	{
+		id: 7,
+		headshot:
+			"https://media.discordapp.net/attachments/1012030681800507492/1029802245279203420/Erin_in_portland.png?width=475&height=634",
+		name: "Erin Halden",
+		title: "Frontend-Web",
+		linkedin: "https://www.linkedin.com/in/erininabox",
+		github: "https://erininabox.com/",
+		discord: "",
+	},
+	{
+		id: 8,
+		headshot:
+			"https://media.discordapp.net/attachments/1012030681800507492/1036396850460229644/Selfie.jpg?width=476&height=635",
+		name: "April Hughes",
+		title: "Fronend-Web",
+		linkedin: "https://www.linkedin.com/in/april-hughes-web-dev/",
+		github: "dreydain - Overview",
+		discord: "April Hughes | Front-end Developer | Seattle, WA",
+	},
+	// {
+	// 	id: 10,
+	// 	headshot: "",
+	// 	name: "",
+	// 	title: "",
+	// 	linkedin: "",
+	// 	github: "",
+	// 	discord: "",
+	// },
 ];
