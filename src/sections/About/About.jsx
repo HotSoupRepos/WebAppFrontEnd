4f5fa0c42d2e68e27009efb024b0c7a1ddefdb84
Simon Bruce <simonbrucepuma@gmail.com>
2023-01-21 13:31:34 -0600

    responsiveness

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -6,15 +6,17 @@ import AppStoreButtons from '../../assets/AppStoreButtons/AppStoreButtons';
 const About = () => {
     return (
         <>
-            <section>
-                <div className='about-text'>
-                    <h1>Connecting those in Need</h1>
-                    <p>When one of us has food insecurity, it is a problem for all of us. Let’s change that, one meal at a time.</p>
-                    <p> We have too much abundance to let each other go hungry, period. </p>
-                    <AppStoreButtons/>
-                </div>
-                <div className='image'>
-                    <img src={UserProfile}/>
+            <section id="About">
+                <div className='section-container'>
+                    <div className='about-text'>
+                        <h1>Connecting those in Need</h1>
+                        <p>When one of us has food insecurity, it is a problem for all of us. Let’s change that, one meal at a time.</p>
+                        <p> We have too much abundance to let each other go hungry, period. </p>
+                        {/* <AppStoreButtons/> */}
+                    </div>
+                    <div className='image'>
+                        <img src={UserProfile} />
+                    </div>
                 </div>
             </section>
         </>
