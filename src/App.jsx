a74f73723d93e1253e39c2222806dd5b9d3d72b8
Simon Bruce <simonbrucepuma@gmail.com>
2023-02-03 16:13:06 -0600

    styling contributors section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PhoneDemo from "./sections/phone-demo/PhoneDemo";
 import Contact from "./sections/Contact/Contact";
 import Contributors from "./sections/Contributors/Contributors";
-import MaterialUIExample from './MaterialUiExample'
 import About from './sections/About/About'
 
 const App = () => {
@@ -12,7 +11,6 @@ const App = () => {
 			<PhoneDemo />
 			<Contact />
 			<Contributors />
-			<MaterialUIExample/>
 		</div>
 	);
 };
